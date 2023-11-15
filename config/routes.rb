3eb7299fe252721263941390fec83ba91aca61f5
2111140049 <2111140049t@ed.fuk.kindai.ac.jp>
2023-11-15 09:22:12 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  get '/new',to: 'session#new',as: 'login'
+  post 'session/new',to: 'session#new'
+  
+  get '/new',to: 'registration#new',as: 'create_I&P'
+  post 'registration/new',to: 'registration#new'
+  
+  get '/index',to: 'home#index'
+  root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
