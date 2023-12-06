34f6b86abf82120bb0fa1b0800d778f80cbfc1da
ShioriMiyazaki <2111140084u@ed.fuk.kindai.ac.jp>
2023-12-06 08:47:50 +0000

    push 12/6

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
-  get 'calender/index'
+  root 'calender#index'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  post "/top_logout_path", to: 'top#top_logout_path'
-  get '/top_logout_path', to: 'top#top_logout_path'
-  
-  get '/login',to: 'session#login',as: 'login'
-  post '/session/login', to: 'session#login'
-  post '/login', to: 'session#login'
-  
-  get '/registration/create_IP',to: 'registration#create_IP',as: 'create_IP'
-  post '/registration/create_IP',to: 'registration#create_IP'
-  post '/create_IP',to: 'registration#create_IP'
-  
-  get '/main',to: 'top#main'
-  root 'top#main'
   # Defines the root path route ("/")
   # root "articles#index"
 end
