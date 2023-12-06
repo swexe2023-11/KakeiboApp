f5844f6a8edc22b5e5533c01cd569baa8ed4d8d0
ShioriMiyazaki <2111140084u@ed.fuk.kindai.ac.jp>
2023-12-06 08:24:51 +0000

    push 12/6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'calender/index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post "/top_logout_path", to: 'top#top_logout_path'
