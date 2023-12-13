315a6f80e735df3ca9fbd7ccc9305100b929d855
2111140049 <2111140049t@ed.fuk.kindai.ac.jp>
2023-12-13 09:59:27 +0000

    12/13 commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,18 @@
 class MainController < ApplicationController
+ def main
+  @Recipt=Recipt.all
+ end
  def InRecipt
     
    render 'InRecipt'
  end
+ def all
+  @Recipt=Recipt.all
+  render 'main/main'
+ end
+ def m1
+  @Recipt=Recipt.where(day: '2023-11-')
+  render 'main/main'
+ end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- 
-
+  
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post "/InRecipt",to: 'main#InRecipt'
   get "/InRecipt",to: 'main#InRecipt'
