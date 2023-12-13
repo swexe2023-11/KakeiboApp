2d8d0faf3941b52e9df89a55e6f333f94ad4cdf4
2111140049 <2111140049t@ed.fuk.kindai.ac.jp>
2023-12-13 05:33:42 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'calender#index'
+ 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post "/InRecipt",to: 'main#InRecipt'
